2f944c29b677c9ccee1c691cf139b4d37b42b764
Yarasi Ashokvardhana Reddy <y.ashokvardhanreddy@gmail.com>
2024-11-14 23:32:46 +0530

    GET Method Added

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,12 +1,30 @@
-// YOUR_BASE_DIRECTORY/netlify/functions/api.ts
 
 import express, { Router } from "express";
+const { MongoClient, ObjectId } = require('mongodb');
+
 import serverless from "serverless-http";
+
+const mongoUri = 'mongodb+srv://yashokvardhanreddy:srff-cluster1995@srff-cluster.dhto4.mongodb.net/sr_frozen_foods?retryWrites=true&w=majority';
+const client = new MongoClient(mongoUri);
+const database = client.db('sr_frozen_foods');
 
 const api = express();
 
 const router = Router();
-router.get("/hello", (req, res) => res.send("Hello World!"));
+// router.get("/hello", (req, res) => res.send("Hello World!"));
+
+router.get("/:collectionName", async (req, res) =>{
+    const { collectionName } = req.params;
+    try {
+        const collection = database.collection(collectionName);
+        const data = await collection.find().toArray();
+        console.log(data)
+        res.json(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+})
 
 api.use("/api/", router);
 
