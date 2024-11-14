0e87df78ea9ba52db1be39b40f31fa108c6866e5
Yarasi Ashokvardhana Reddy <y.ashokvardhanreddy@gmail.com>
2024-11-14 23:57:57 +0530

    .

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -3,30 +3,33 @@ import express, { Router } from "express";
 import { MongoClient, ObjectId } from 'mongodb';
 
 import serverless from "serverless-http";
+// const PORT = process.env.PORT || 5000;
 
 const mongoUri = 'mongodb+srv://yashokvardhanreddy:srff-cluster1995@srff-cluster.dhto4.mongodb.net/sr_frozen_foods?retryWrites=true&w=majority';
 const client = new MongoClient(mongoUri);
 const database = client.db('sr_frozen_foods');
 
+// console.log(database);
 
 const api = express();
 
 const router = Router();
-// router.get("/hello", (req, res) => res.send("Hello World!"));
+api.get("/hello", (req, res) => res.send("Hello World!"));
 
-router.get("/:collectionName", async (req, res) =>{
+api.get("/api/:collectionName", async (req, res) =>{
     const { collectionName } = req.params;
     try {
         const collection = database.collection(collectionName);
         const data = await collection.find().toArray();
         console.log(data)
-        res.json(data);
+        res.send(data);
     } catch (error) {
         console.error('Error fetching data:', error);
         res.status(500).json({ message: 'Internal Server Error' });
     }
 })
 
-api.use("/api/", router);
 
+
+// api.use("/api/", router);
 export const handler = serverless(api);
